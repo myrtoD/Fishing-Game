bbeda5f5e72f355eea7da6f29d7d3e09ea4d02d4
Myrto <diakv115@newschool.edu>
2015-10-13 19:14:17 +0300

    somewhat there

---
+++ Fishing Game/Assets/Mover.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+		
+		private float speed;
+		private float xSpeed;
+		private float zSpeed;
+		
+		
+		void Start () {
+			
+			speed = 5f;
+			
+			
+		}
+		
+		void Update () {
+			xSpeed = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+			zSpeed = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+			
+			transform.Translate (Vector3.forward * xSpeed);
+			transform.Translate (Vector3.down * zSpeed);
+			
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				
+				transform.Translate (Vector3.left * speed * Time.deltaTime);
+				
+			}
+			
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				
+				transform.Translate (Vector3.right * speed * Time.deltaTime);
+				
+			}
+			
+		}
+	}
---
+++ Fishing Game/Assets/fishMover3.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishMover3 : MonoBehaviour {
+
+	private Transform startPoint, endPoint;
+	public float percentage;
+	public float speed = 2;
+	
+	private int direction;
+	
+	void Start () {
+		startPoint = GameObject.Find ("startpoint").transform;
+		endPoint = GameObject.Find ("endpoint").transform;
+		direction = 1;
+	}
+	
+	void Update () {
+		transform.position = Vector3.Lerp (startPoint.position, endPoint.position, percentage);
+		
+		speed = Mathf.Max (speed, .000001f);
+		percentage += Time.deltaTime/speed * direction;
+		
+		
+		if ((percentage > 1) || (percentage<0)) {
+			direction = -direction;
+			percentage = Mathf.Clamp (percentage, 0, 1);
+		}
+	}
+}
